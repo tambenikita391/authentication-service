868baad2e30ab582573603fd81ceda2f7802eab4
tambenikita391 <tambenikita391@gmail.com>
2024-04-05 21:43:19 +0530

    feat: Add email in pre-commit

---
+++ Product.java
@@ -0,0 +1,15 @@
+public class Product {
+  String firstName;
+  String lastName;
+  String gender;
+  int age;
+  String email;
+
+  public void printUser() {
+    System.out.println("First Name :" + firstName);
+    System.out.println("Last Name :" + lastName);
+    System.out.println("Gender :" + gender);
+    System.out.println("Age :" + age);
+    System.out.println("Email :" + email);
+  }
+}
